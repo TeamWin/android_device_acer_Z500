2d4f1ef87baec06736cb0936153e2e58b4a04999
hejsekvojtech <hejsekvojtech@gmail.com>
2017-05-12 15:04:33 +0200

    Remove unwanted qcom files
    Change-Id: I79d84eb5118377abb9766c5ac1247f827fdbd38a

--- device.mk
+++ device.mk
@@ -10,6 +10,16 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/kernel:kernel \
     $(LOCAL_PATH)/recovery/sbin/healthd:root/sbin/healthd
 
+PRODUCT_COPY_FILES_OVERRIDES += \
+    root/fstab.qcom \
+    root/init.qcom.sh \
+    root/remount.qcom \
+    root/ueventd.qcom.rc \
+	recovery/root/fstab.qcom \
+    recovery/root/init.qcom.sh \
+    recovery/root/remount.qcom \
+    recovery/root/ueventd.qcom.rc \
+
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_NAME := Z500
